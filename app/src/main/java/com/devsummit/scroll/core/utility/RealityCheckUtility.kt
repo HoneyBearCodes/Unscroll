ed70c38a4a51c2352ad49004fc23aec053dfcc1a
Shashank Singh <96770927+HoneyBearCodes@users.noreply.github.com>
2026-03-27 07:18:23 +0530

    chore(utility): replace repetitive Pages Read with Podcast Episodes

--- app/src/main/java/com/devsummit/scroll/core/utility/RealityCheckUtility.kt
+++ app/src/main/java/com/devsummit/scroll/core/utility/RealityCheckUtility.kt
@@ -17,7 +17,7 @@ object RealityCheckUtility {
             Achievement("Exercise", (hours * 500.0), "Calories Burned"),
             Achievement("Language", (minutes / 15.0), "Languages Lessons"),
             Achievement("Walking", (minutes / 20.0), "Miles Walked"),
-            Achievement("Learning", (minutes / 1.5), "Pages Read"),
+            Achievement("Learning", (minutes / 45.0), "Podcast Episodes"),
             Achievement("Entertainment", (hours / 2.0), "Movies Watched")
         )
     }
